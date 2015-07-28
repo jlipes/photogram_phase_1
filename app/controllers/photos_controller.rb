bff158d09824677be7215a801702838a1c85bf29
jlipes <jason.lipes@gmail.com>
2015-07-28 09:13:09 -0500

    phase 1 complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,21 @@
 class PhotosController < ApplicationController
+
   def index
     @list_of_photos = Photo.all
   end
 
+  def show
+    @selected_photo=Photo.find(params["id"])
+  end
+
+  def destroy
+    @selected_photo=Photo.find(params["id"])
+    @selected_photo.destroy
+    redirect_to("/photos.html")
+  end
+
+  def new_form
+
+  end
 
 end
